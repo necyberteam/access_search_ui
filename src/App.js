1cbe86d887a63f00a5e7f189d8558700a338f3fc
@FloQuinn <ingmann33@gmail.com>
2022-08-12 13:31:54 -0400

    Hide "hide/show search" button until first search

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ export default function App() {
                   header={
                     <>
                       <SearchBox autocompleteSuggestions={true} />
-                      <CollapseBut></CollapseBut>
+                      {wasSearched && <CollapseBut></CollapseBut>}
                     </>
                   }
 
